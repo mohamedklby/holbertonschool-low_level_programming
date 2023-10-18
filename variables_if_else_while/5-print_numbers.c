a35b474ea8da30c78c8bccbe2a39f7cbcfbd28b5
mohamedklby <moha.klby333@gmail.com>
2023-10-18 06:51:58 -0700

    haller

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (if success)
+ */
+
+int main(void)
+{
+	int nombre;
+	for (nombre = 0; nombre <= 10; nombre++)
+		printf("%d\n", nombre);
+	return (0);
+}
