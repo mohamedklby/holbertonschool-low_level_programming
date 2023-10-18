786a5d5c6d8156fa03d73876683ff5c47a86ede0
mohamedklby <moha.klby333@gmail.com>
2023-10-18 06:25:11 -0700

    haller

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Rreturn: 0 (if success)
+ */
+
+int main(void)
+{
+	char ronaldo;
+
+	for (ronaldo = 'a' ; ronaldo <= 'z' ; ronaldo++)
+		if (ronaldo != 'e' && ronaldo != 'q')
+			putchar(ronaldo);
+	putchar('\n');
+	return (0);
+}
