295f81219b3698b4fc9c2cb49546637d9297a75c
mohamedklby <moha.klby333@gmail.com>
2023-10-18 08:00:35 -0700

    torres

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,8 @@
 int main(void)
 {
 	int nombre;
-	for (nombre = 0; nombre < 10; nombre++)
-	{	
-		printf("%d", nombre);
-	}	
-
+	for (nombre = 0; nombre <= 10; nombre++)
+	printf("%d", nombre);
 	printf("\n");
 
 	return (0);
