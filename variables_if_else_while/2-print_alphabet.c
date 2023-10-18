30fc368de3995dfdc600b5de5b6bdb3374ed7aba
mohamedklby <moha.klby333@gmail.com>
2023-10-18 01:28:04 -0700

    henry

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return 0 (if success)
+ */
+
+int main(void)
+{
+	char lettre;
+
+	for (lettre = 'a' ; lettre <= 'z' ; lettre++)
+	{
+		putchar(lettre);
+	putchar("\n");
+	return (0);
+}
