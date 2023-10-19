f4e2d0865172a437adf4cd91378d055dca9e0333
mohamedklby <moha.klby333@gmail.com>
2023-10-19 06:06:19 -0700

    salandro

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - program that prints _putchar
  *
  * Return: Always 0
  */
-
+ 
 int main(void)
 {
-	char _putchar;
 
-	putchar("_")
-	putchar("p")
-	putchar("u")
-	putchar("t")
-	putchar("c")
-	putchar("h")
-	putchar("a")
-	putchar("r")
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
