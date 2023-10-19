7f89d4ee1fb5e9ca0be57209d0b9c388e85f86d7
mohamedklby <moha.klby333@gmail.com>
2023-10-19 03:22:22 -0700

    ronaldinhor

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - program that prints _putchar.c
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int _putchar.c;
+
+	printf("_putchar.c\n")
+	printf("_putchar.c");
+
+	return 0;
+}
