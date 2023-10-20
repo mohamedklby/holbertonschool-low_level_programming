21edc7167d2b19ed17de295a4ed54e3960551893
mohamedklby <moha.klby333@gmail.com>
2023-10-20 02:45:21 -0700

    zizouninho

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet();
+ * prints_alphabet - entry point
  *
  * Return: always 0
  */
 
 void print_alphabet(void);
-{
+{	
 	char alphabet;
-	{
-		for (alphabet in range(97, 122):
-		putchar(chr('alphabet'));
-	}	
+
+		for (alphabet = 'a'; alphabet = 'z'):
+		putchar(alphabet);
+		putchar('\n')
 	return (0);
 }
