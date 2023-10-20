103a30054c3fbf2a58cc0312a013cf4857f79ff8
mohamedklby <moha.klby333@gmail.com>
2023-10-20 02:33:39 -0700

    zouzinho

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void);
+{
+	char alphabet;
+	{
+		for (alphabet in range(97, 122):
+		putchar(chr(alphabet));
+	}	
+	return (0);
+}
