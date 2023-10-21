618edba699dfce4dc4388fce9de0fbacf36c3eb0
mohamedklby <moha.klby333@gmail.com>
2023-10-21 12:32:27 -0700

    rakitic

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -  prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char = alpha;
+	for (alpha = a; alpha =< z; alpha++)
+		_putchar(char c);
+	putchar('\n')
+}
