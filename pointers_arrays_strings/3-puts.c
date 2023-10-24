03db5544e496745cce4289ff783d987cc3962e73
mohamedklby <moha.klby333@gmail.com>
2023-10-24 03:53:51 -0700

    gohou

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line.
+ * @str : string to print
+ * Return: i
+ */
+
+void _puts(char *str)
+{
+	int vip;
+
+	l = 0;
+	while (vip[l] != '\0')
+		_putchar(vip[l]);
+	l++;
+	putchar ('\n');
+}
