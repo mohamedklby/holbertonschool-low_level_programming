12ce37480d639ec213270c38d9dc3fa51e6ca87b
mohamedklby <moha.klby333@gmail.com>
2023-10-25 02:55:02 -0700

    rl

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - entry point
+ *
+ * @str: pointer to an string
+ * Description: function that prints every other character of a string.
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+int lettre;
+
+for (lettre = 0; str[lettre] != '\0'; lettre++)
+{
+if (lettre % 2 == 0)
+_putchar(str[lettre]);
+}
+_putchar('\n');
+} 
