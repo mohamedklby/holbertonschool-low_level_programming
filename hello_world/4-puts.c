661a2fc1cff300acdd22fd93e1625bfd9debf84d
mohamedklby <moha.klby333@gmail.com>
2024-10-21 15:13:24 +0000

    jjhh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(void) {
-	puts("hello");
-	return 0;
+int main(void)
+{
+puts("hello");
+return (0);
 }
