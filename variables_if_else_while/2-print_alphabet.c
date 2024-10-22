dfd464cdbf5e10971089353de9dc46c681c7b245
mohamedklby <moha.klby333@gmail.com>
2024-10-22 18:55:27 +0000

    kj

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+char letter = 'a';
 
-	char letter = 'a';
-	
-	while (letter <= 'z')
-	{
-	    putchar(letter);
-	    letter++;
-	}
-	putchar('\n');
-	return(0);
+while (letter <= 'z')
+
+putchar(letter);
+letter++;
+
+putchar('\n');
+return (0);
 }
