f2ef9a2883e1dbfbf0cb63e58fe2997a210ffedc
mohamedklby <moha.klby333@gmail.com>
2024-10-22 14:45:46 +0000

    j

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,16 +10,19 @@
 
 int main(void)
 {
-	int n;
+	int n, d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	d = n % 10;
 
-	if (n > 5)
-		printf("%d is greater than 5 \n", n);
-	else if (n == 0)
-		printf("%d is zero \n", n);
+	printf("Dernier chiffrre de %d est %d", n, d);
+
+	if (d > 5)
+		printf("est superieur a 5 \n");
+	else if (d == 0)
+		printf("et est 0\n");
 	else
-		printf("%d is less than 6 and not 0 \n", n);
+		printf("est inferieur a 6 et non a 0\n");
 	return (0);
 }
