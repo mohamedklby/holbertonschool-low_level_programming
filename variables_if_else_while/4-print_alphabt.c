713a227f08e5e426c0e8d564963130480fd9a627
mohamedklby <moha.klby333@gmail.com>
2024-10-23 10:22:14 +0000

    jk

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		if (letter != 'q' && 'e')
+		if (letter != 'q' && letter != 'e')
 			putchar(letter);
 
 	putchar('\n');
