d1871339d432329201dd2bfb52101b0e45ece4c0
mohamedklby <moha.klby333@gmail.com>
2024-10-23 13:13:40 +0000

    kjk

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int nombre;
+
+for (nombre = 0; nombre < 10; nombre++)
+
+putchar (nombre + '0');
+
+putchar('\n');
+
+return (0);
+}
