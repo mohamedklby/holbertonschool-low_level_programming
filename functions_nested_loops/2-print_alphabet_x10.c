892129d0317d9eee42546a7bb892c2603e7e9b8f
mohamedklby <moha.klby333@gmail.com>
2024-10-25 09:56:12 +0000

    kjjk

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,11 @@
 void print_alphabet_x10(void)
 
 {
-	char alpha;
-	int i;
 
-	for (i = 0; i <= 10; i++)
+	int i;
+	char alpha;
+
+	for (i = 0; i < 10; i++)
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 
 		_putchar(alpha);
