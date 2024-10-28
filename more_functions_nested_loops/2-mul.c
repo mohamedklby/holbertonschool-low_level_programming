8615bec1860ba310dee60cf5d4749cdbee2a141c
mohamedklby <moha.klby333@gmail.com>
2024-10-28 16:06:59 +0000

    kjkj

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies deux entiers
+ * @a: le premier entier
+ * @b: le deuxiÃme entier
+ *
+ * Return: always 0 (SuccessÂ°
+ */
+
+int mul(int a, int b)
+{
+
+	printf("%d\n", 784 * 128);
+	printf("%d\n", -1024 * 1608);
+
+		return (a && b);
+
+	return (0);
+}
