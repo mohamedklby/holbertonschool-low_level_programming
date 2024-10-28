4fc280ecae2750daba61d8a83d5ac4b1f2a4718b
mohamedklby <moha.klby333@gmail.com>
2024-10-28 15:16:45 +0000

    kjkjjk

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a caracter is uppercase
+ * @c: character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+			return (0);
+}
