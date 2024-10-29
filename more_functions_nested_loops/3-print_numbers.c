303d4f0792f5aa2aefd19312497a43cd593dc512
mohamedklby <moha.klby333@gmail.com>
2024-10-29 14:03:02 +0000

    kjhj

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
 
 void print_numbers(void)
 {
-    int num;
-    
-    for (num = '0'; num <= '9'; num++)
-        _putchar(num);
-    _putchar('\n');
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		_putchar(num);
+	_putchar('\n');
 }
 
