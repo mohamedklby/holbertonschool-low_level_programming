469ec57fc3da7d15a0b30b68f96a44d0806178dc
Your Name <you@example.com>
2024-10-30 14:38:52 +0000

    hjbhbb

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * swap_int - function that swaps the values.
  *
  * @a: premier entier.
- * @b: euxieme entier.
+ * @b: deuxieme entier.
+ *
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = *b;
-	*b = temp;
-	swap_int(&*a, &*b);
+	int temp;
+
+	temp = *b;
+	*b = *a;
+	*a = temp;
 }
