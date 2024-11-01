078511a5ba8e7b8f76d089df473517016e607807
mohamedklby <moha.klby333@gmail.com>
2024-11-01 16:11:27 +0000

    klkjhjv

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare deux chaînes de caractères
+ * @s1: Première chaîne de caractères
+ * @s2: Seconde chaîne de caractères
+ *
+ * Return: 0 si les chaînes sont identiques,
+ *         < 0 si s1 est inférieure à s2,
+ *         > 0 si s1 est supérieure à s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
