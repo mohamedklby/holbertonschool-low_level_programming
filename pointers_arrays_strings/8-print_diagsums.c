a0b831c332f46b1a901aaec1d687896da237b1f6
mohamedklby <moha.klby333@gmail.com>
2024-11-06 11:20:04 +0000

    kljhgh

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - Calcule et affiche la somme des deux diagonales
+ * @a: Pointeur vers la matrice
+ * @size: Taille de la matrice carré
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
+
