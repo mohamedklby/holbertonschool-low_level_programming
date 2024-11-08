5d1b08973706bcb4aebb431bd400b163e1a78d48
mohamedklby <moha.klby333@gmail.com>
2024-11-08 14:02:56 +0000

    lkjhg

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - function to print multiplication result
- * @argc: number of arguments
- * @argv: arguments supplied
- * Return: always 1 if less than two arg var supplied otherwise 0.
+ * main - function to print all arguments being passed
+ * @argc: argument numbers
+ * @argv: arguments passed
+ * Return: Always 0
  */
-
 int main(int argc, char **argv)
 {
-	int a = 0, b = 0;
+	int a;
 
-	if (argc > 2)
+	for (a = 0; a < argc; a++)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-	}
-	else
-	{
-		printf("Error\n");
+		printf("%s\n", argv[a]);
 	}
 	return (0);
 }
