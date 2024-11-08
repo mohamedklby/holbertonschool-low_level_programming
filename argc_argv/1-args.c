97ac8e049264406af91ba0fd11598813c5f7ad28
mohamedklby <moha.klby333@gmail.com>
2024-11-08 14:01:26 +0000

    lkjhg

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - function to print file name
- * @argc: count of the arguments supplied to the program and the
- * @argv: an array of pointers to the strings
+ * main - function
+ * @argc: number of arguments passed
  * Return: Always 0
+ * @argv: arguments passed here
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
