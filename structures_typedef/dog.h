9eeb498007f662a6a26430b5b02a13d9255d8d3b
mohamedklby <moha.klby333@gmail.com>
2024-11-15 21:53:51 +0000

    lkj

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure pour d√finir les propri√t√s d'un chien
+ * @name: nom du chien.
+ * @owner: propri√taire du chien.
+ * @age: age du chien.
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
+
