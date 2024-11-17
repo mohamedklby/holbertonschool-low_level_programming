4c2ce3b8dc66b7b419f510c2cebc9364e8742e67
mohamedklby <moha.klby333@gmail.com>
2024-11-17 19:57:57 +0000

    kjhgg

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,5 +15,14 @@ struct dog
 	float age;
 };
 
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to struct dog to initialize
+ * @name: Name to initialize
+ * @age: Age to initialize
+ * @owner: Owner to initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
 
