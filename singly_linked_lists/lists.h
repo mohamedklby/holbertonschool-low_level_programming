3697e4eaed01c6aba797cf044e95932c8f237221
mohamedklby <moha.klby333@gmail.com>
2024-12-05 21:04:48 +0000

    jhg

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stddef.h>
 
 /**
@@ -11,15 +10,22 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
-    char *str;          /* Chaîne de caractères */
-    unsigned int len;   /* Longueur de la chaîne */
-    struct list_s *next;/* Pointeur vers le prochain nœud */
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
-
-/* Prototype de la fonction print_list */
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
+
+
+
+
+
+
+
